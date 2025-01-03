ae98d5d0b09102cc0d361a141a927ccf0e65ccab
Ackjosh <163001837+Ackjosh@users.noreply.github.com>
2025-01-03 22:31:56 +0530

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,4 +1,4 @@
-let currentSong = new Audio();//declaring it to chnage it afterwards
+let currentSong = new Audio();//declaring it to change it afterwards
 let currFolder;
 
 function formatTime(seconds) {
